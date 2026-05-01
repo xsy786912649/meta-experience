6f1b56e312c3598bdf992c38fde2e0f4309ba6bb
xsy786912649 <786912649@qq.com>
2026-05-01 17:42:17 -0400

    Update run_meta_grpo_qwen3_common.sh

--- training/bfcl_verl_meta_rl_new/recipe/bfcl_meta/run_meta_grpo_qwen3_common.sh
+++ training/bfcl_verl_meta_rl_new/recipe/bfcl_meta/run_meta_grpo_qwen3_common.sh
@@ -45,7 +45,7 @@ python3 -m verl.trainer.main_ppo \
   data.train_files="$TRAIN_FILES" \
   data.val_files="$VAL_FILES" \
   actor_rollout_ref.model.path="$MODEL_PATH" \
-  actor_rollout_ref.actor.optim.lr=1e-6 \
+  actor_rollout_ref.actor.optim.lr=5e-7 \
   actor_rollout_ref.actor.optim.lr_warmup_steps_ratio="$LR_WARMUP_STEPS_RATIO" \
   actor_rollout_ref.model.use_remove_padding=True \
   actor_rollout_ref.actor.ppo_mini_batch_size=32 \
